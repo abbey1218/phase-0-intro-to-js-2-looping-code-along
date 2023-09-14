0868c8dab5b4012a95f78c6e86c79d21feeff82d
Abbey Laudadio <laudadioabbey@gmail.com>
2023-09-14 13:14:36 -0500

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+function writeCards(names) {  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    
+    return messages;
+  };
+
+const count = [];
+function countDown() {
+    for (let count = 10; count > -1; count--) {
+        console.log(count);
+    }
+};
+
